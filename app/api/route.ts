2781d84dba362ffcb041eea7dc1f8edb7a4930a8
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-01-30 21:50:19 +0100

    adding api methods test

---
+++ app/api/route.ts
@@ -0,0 +1,11 @@
+//  import type { NextApiRequest, NextApiResponse } from "next"
+// import { NextRequest } from "next/server";
+
+export async function GET(req: Request, res:Response){
+    return Response.json({message:"hello ayo"});
+}
+
+export async function POST(req:Request, res:Response){
+    const data = await req.json();
+    return  Response.json(data);
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+
+export default  function Navbar(){
+   const router = useRouter();
+   const path = usePathname();
+    return (
+        <>
+
+        </>
+    )
+}
