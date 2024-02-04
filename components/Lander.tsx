30ef7bf73c5eb3dd44917b939df3297588ca1396
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-02-04 19:52:17 +0100

    changed fonts

--- components/Lander.tsx
+++ components/Lander.tsx
@@ -2,7 +2,11 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Single_Day } from "next/font/google";
+import { Gupter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
+const mont = Montserrat({weight:["600"], subsets:['cyrillic-ext'], style:['normal']})
+const gupter = Gupter({weight:['500'], subsets:['latin'], style:['normal']});
 const single = Single_Day({weight:["400"], style:['normal']});
 export default function Lander(){
     return (
@@ -13,13 +17,14 @@ export default function Lander(){
              animate={{opacity:1, y:0}}
              transition={{duration:0.7}}
              >
-                  <p className={`text-6xl font-bold text-white  ${single.className}`}>Hi <span className="text-3xl font-bold text-gray-400">,</span>I Am  Ayo </p>
+                  <p className={`text-6xl font-bold text-white  ${mont.className} p-3`}>Hi <span className="text-3xl font-bold text-gray-400">,</span>I Am  Ayo </p>
              </motion.div>
              <motion.div
              initial={{opacity:0, y:30}}
              animate={{opacity:1, y:0}}
+             transition={{duration:0.7}}
              >
-                 <p className="text-xl font-medium text-gray-500">Web developer from lagos, Nigeria</p>
+                 <p className={`text-3xl font-medium text-gray-500 ${single.className} p-3`}>Web developer from lagos, Nigeria</p>
              </motion.div>
         </div>
         {/* <div className="hidden lg:block md:block">
