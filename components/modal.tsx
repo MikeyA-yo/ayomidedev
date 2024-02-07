19b6ef4bb133c7c381fbfefaaf6ce1f467d5fc7a
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-02-07 22:51:52 +0100

    really minor changes

--- components/modal.tsx
+++ components/modal.tsx
@@ -19,7 +19,6 @@ useMotionValueEvent(scrollY, "change", (latest) => {
             delay={200}
           className={`fixed bottom-4 right-4  bg-blue-500 hover:bg-transparent hover:text-blue-500 hover:border-blue-500 hover:border-2 text-white font-bold py-2 px-4 rounded-full ${scrollPosition > 400 ?'opacity-100' : 'opacity-0'} shadow-lg`}
         >
-          {/* Add your icon or text here */}
           <div className="animate-bounce">  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
                  </svg>
