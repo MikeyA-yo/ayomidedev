e6051e9193a743fb06593ccd3ca258d8d0dd22e2
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-02-20 20:01:35 +0100

    minor changes

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,10 +6,10 @@ export default function Profile(){
         <>
           <div  id="profile">
               <div className="pb-24"></div>
-              <h1>Profiles</h1>
+              <h1 className="text-center text-3xl pb-14">PROFILES</h1>
               <hr />
               <div className="flex">
-
+                
               </div>
           </div>
         </>
