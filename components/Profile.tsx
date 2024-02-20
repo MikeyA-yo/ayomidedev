8e5c1d87a11ce445d3931704c4bd24e4e57fbcfe
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-02-20 21:19:28 +0100

    in my opinion i'm true with profiles i don't know what to add, but i think that's all the profiles i have when i create linkenldn i will add it to

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,15 +1,33 @@
-import { RegularGithub, RegularTwitter, RegularTwitterFill } from "lineicons-react";
+"use client";
+import { RegularGithub, RegularTwitter, RegularTwitterFill, RegularTwitterOriginal } from "lineicons-react";
 import { motion } from "framer-motion";
 
 export default function Profile(){
+    const variants={
+        hover:{
+            scale:[1,0.7,1]
+        }
+    }
     return (
         <>
           <div  id="profile">
               <div className="pb-24"></div>
               <h1 className="text-center text-3xl pb-14">PROFILES</h1>
-              <hr />
-              <div className="flex">
-                
+              <hr className="pb-14"/>
+              <div className="flex gap-5 justify-center">
+                <motion.div
+                variants={variants}
+                whileHover={`hover`}
+                >
+                     <a href="https://github.com/MikeyA-yo" className="hover:bg-blue-500"><RegularGithub className="h-24 w-24 hover:fill-white hover:bg-blue-500" /></a>
+                </motion.div>
+                <div className=" border-r-2"></div>
+                <motion.div
+                variants={variants}
+                whileHover={`hover`}
+                >
+                     <a href="https://x.com/OluwatolaAyomi4?t=GQoEQpDSRlm7pyPLPxQHhA&s=09" className="hover:bg-blue-500"><RegularTwitter className="h-24 w-24 fill-blue-500 hover:bg-blue-500 hover:fill-white" /></a>
+                </motion.div>
               </div>
           </div>
         </>
