8837887f78fc5f2ef92d5f45c14299b2f054c23b
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-02-25 21:04:56 +0100

    i'm tired

--- components/JS/links.js
+++ components/JS/links.js
@@ -1,1 +1,6 @@
 const diceGame = 'https://dicegame-dun-eta.vercel.app/';
+const drumKit = 'https://drumkitjs-nine.vercel.app';
+const choCountries = 'https://cho-countries.vercel.app/';
+const animeVault = 'https://recipe-book-lemon.vercel.app';
+
+export  const projects = [diceGame, drumKit, choCountries, animeVault];
