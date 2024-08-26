6d22646e128a13e2d61b4a724d7121363cdd9ea6
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-08-26 14:55:15 +0100

    i guess she is right

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,12 +2,22 @@
 export default function Contact(){
     return (
         <>
-            <div className="flex flex-col gap-2 mt-4" id="contact">
-                <div className="flex">
-                  
+            <div className="flex flex-col gap-2 items-center justify-center mt-4" id="contact">
+                <h3>Contact Me</h3>
+               <div className="flex flex-col gap-2 items-center">
+               <div className="flex gap-2">
+                   <div className="flex flex-col gap-1">
+                    Name:
+                    <input className="p-2" />
+                   </div>
+                   <div className="flex flex-col gap-1">
+                    Email:
+                    <input className="p-2" />
+                   </div>
                 </div>
                 <div></div>
                 <div></div>
+               </div>
             </div>
         </>
     )
