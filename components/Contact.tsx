503b45eff2e6fca2b39ba71d2053e0bab428f2d6
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-11 19:34:49 +0100

    to add UI for confirmation in contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,24 +1,72 @@
 "use client";
-export default function Contact(){
-    return (
-        <>
-            <div className="flex flex-col gap-2 items-center justify-center mt-4" id="contact">
-                <h3>Contact Me</h3>
-               <div className="flex flex-col gap-2 items-center">
-               <div className="flex gap-2">
-                   <div className="flex flex-col gap-1">
-                    Name:
-                    <input className="p-2" />
-                   </div>
-                   <div className="flex flex-col gap-1">
-                    Email:
-                    <input className="p-2" />
-                   </div>
-                </div>
-                <div></div>
-                <div></div>
-               </div>
+
+import { useState } from "react";
+
+export default function Contact() {
+    const [data, setData] = useState({name:"", email:"", message:""});
+    const handleInput = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        setData(prev => {
+            return {
+                ...prev,
+                [e.target.name] : e.target.value
+            }
+        })
+    }
+    const handleMessage = (e:React.ChangeEvent<HTMLTextAreaElement>) =>{
+        setData(prev =>{
+            return {
+                ...prev,
+                [e.target.name] : e.target.value
+            }
+        })
+    }
+    async function sendMessage(data:{name:string, email:string, message:string}) {
+        if(data.name.length < 2 || data.email.length < 4 || data.message.length === 0){
+            return
+        }
+        const jsonData = JSON.stringify(data)
+        const res = await fetch("/api", {
+            method:"POST",
+            body:jsonData
+        })
+        if(res.ok){
+            return
+        }
+    }
+  return (
+    <>
+      <div
+        className="flex flex-col gap-2 items-center justify-center mt-4"
+        id="contact"
+      >
+        <h3 className="text-2xl lg:text-3xl font-bold">Contact Me</h3>
+        <div className="flex flex-col gap-4 items-center bg-[#2E073F] p-4 text-[#EBD3F8]">
+          <div className="flex lg:flex-row md:flex-row flex-col gap-2">
+            <div className="flex flex-col gap-2">
+              Name:
+              <input onChange={handleInput} name="name" className="p-2 text-[#AD49E1] rounded" />
             </div>
-        </>
-    )
+            <div className="flex flex-col gap-2">
+              Email:
+              <input onChange={handleInput} name="email" className="p-2 text-[#AD49E1] rounded" />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            Message:
+            <textarea
+            onChange={handleMessage}
+              name="message"
+              placeholder="Type in your Message..."
+              className="min-h-24 p-2 text-[#AD49E1]"
+            />
+          </div>
+          <div>
+            <button onClick={()=>{
+                sendMessage(data)
+            }} className="bg-[#7A1CAC] rounded p-2">Send Message</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
