98b0741f34a2ec6e8ce27fdb7a738952dbeb5c62
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-16 21:10:14 +0100

    got rid of em..

--- components/Lander.tsx
+++ components/Lander.tsx
@@ -1,5 +1,4 @@
 import { Single_Day } from "next/font/google";
-import { Gupter } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import { MotionDiv } from "./Motion";
 
@@ -8,16 +7,13 @@ const mont = Montserrat({
   subsets: ["cyrillic-ext"],
   style: ["normal"],
 });
-// const gupter = Gupter({weight:['500'], subsets:['latin'], style:['normal']});
 const single = Single_Day({ weight: ["400"], style: ["normal"] });
 const singday = single.className;
 export default function Lander() {
   return (
-    // gap-6 items-center justify-center
     <>
       <div className="min-h-screen relative  " id="lander">
         <div className="hidden lg:block md:block">
-          {/* <img src="/pc.jpg" alt="pc image" className="min-h-screen" /> */}
           <div className="flex h-screen w-full items-center justify-center mb bg-cover bg-center">
           <div className="flex flex-col gap-6 items-center justify-center w-full">
               <MotionDiv
@@ -48,7 +44,6 @@ export default function Lander() {
           </div>
         </div>
         <div className="block lg:hidden md:hidden">
-          {/* <img src="/m2.jpg" alt="pc image" className="h-screen" /> */}
           <div className="flex h-screen w-full items-center justify-center mb bg-cover bg-left">
             <div className="flex flex-col gap-6 items-center justify-center w-full ">
               <MotionDiv
@@ -72,7 +67,7 @@ export default function Lander() {
                 <p
                   className={`text-5xl font-medium text-blue-700 ${singday} p-3`}
                 >
-                  Web developer.
+                  Web Developer.
                 </p>
               </MotionDiv>
             </div>
@@ -81,11 +76,4 @@ export default function Lander() {
       </div>
     </>
   );
-}
-
-//
-{
-  /* <div className="absolute top-1/2 left-1/2 mb bg-cover bg-center transform -translate-x-1/2 -translate-y-1/2 w-full ">
-            
-</div> */
 }
